9cf8a921e424a77dc4316767b33bf57a5032c4f5
Medhasri Veldurthi <medhasri.vel@gmail.com>
2023-01-31 16:52:31 -0500

    class info

--- src/Student.java
+++ src/Student.java
@@ -1,6 +1,5 @@
-public class Student {
-    int id;
-    String name;
-
-
+public class Student implements Comparable<Student> {
+    private Profile profile;
+    private Major major; //Major is an enum type
+    private int creditCompleted;
 }
